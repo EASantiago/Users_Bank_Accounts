8d2d4a573c474642dab8d82d298d1810e6aa3537
Santie <santie12@live.com>
2021-04-07 22:31:48 -0500

    initial commit

---
+++ users_bank_accounts.py
@@ -0,0 +1,48 @@
+class BankAccount:
+    def __init__(self, int_rate=0, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount 
+        else:
+            self.balance -= 5 
+            print("Insufficient Funds: Charging a $5 fee")
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+        return self
+
+    def yield_interest(self):
+        self.balance += self.balance * self.int_rate
+        return self
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_user_balance(self):
+        print(f"User:{self.name}, Balance: ${self.account.balance}")
+        return self
+
+john = User("John Doe", "johndoe@doe.com")
+
+john.make_deposit(500).make_withdrawal(100).display_user_balance()
+john.account.yield_interest().display_account_info()
+john.display_user_balance()
